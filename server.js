135fdbd7f411d455f4425d46652a65884dc8170d
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-08-13 16:51:40 +0500

    files uploaded

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(cors(corsOptions));
 app.get('/wc/ping/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const { ts, auth, rwcToken, dmz, url } = req.query;
+        const { ts, auth, rwcToken, dmz, v1, v2 } = req.query;
 
-        if (!ts || !auth || !rwcToken || !dmz || !url) {
+        if (!ts || !auth || !rwcToken || !dmz || !v1 || !v2) {
             return res.status(400).json({ error: "Missing required query parameters." });
         }
 
-        const url_ = `https://${url}/wc/ping/${id}?ts=${ts}&auth=${auth}&rwcToken=${rwcToken}&dmz=${dmz}`;
-
+        const url_ = `https://${v1}.${v2}.zoom.us/wc/ping/${id}?ts=${ts}&auth=${auth}&rwcToken=${rwcToken}&dmz=${dmz}`;
+        console.log(url_)
         const response = await axios.get(url_);
         
         // Sending the response data as JSON
@@ -33,5 +33,5 @@ app.get('/wc/ping/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
